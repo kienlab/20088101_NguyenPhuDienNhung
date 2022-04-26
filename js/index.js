f97ac21f0fd1b2b2d11a1370ce911e74c414e2d6
kienlab <104433556+kienlab@users.noreply.github.com>
2022-04-26 20:21:22 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,140 +4,138 @@ $(document).ready(function(){
     });
 
     //============= HANDLE LOGIC HERE =============//
+	
+    $("#txtName").blur(checkName);
+    $("#txtNum").blur(checkNum);
+    $("#txtClb").blur(checkClb);
+    $("#txtDate").blur(checkDate);
+    $("#txtPhone").blur(checkPhone);
+    $('#txtImage').blur(checkImage);
 
-    // validation name
-    function checkName () {
-        var name = $("#txtName").val();
-        var errorName = $("#error_name");
-        var regexName = /^[A-Z][a-zA-Z]*(\s[A-Z][a-zA-Z]*)*$/;
-        if (name == "" || name == null) {
-            errorName.html("Họ và tên không được để trống!");
+
+	function validActions(id, iderr, regex, null_str, fail_str)
+	{
+        var val = $(id).val();
+        var errorName = $(iderr);
+        if (val == "" || val == null) {
+            errorName.html(null_str);
             return false;
-        } else if (!regexName.test(name)) {
-            errorName.html("Họ và tên không hợp lệ!");
+        } else if (!regex.test(val)) {
+            errorName.html(fail_str);
+            return false;
+        } else {
+            errorName.html("");
+            return true;    // return true if username is valid
+        }
+	}
+
+    function checkName () {
+        return validActions(
+			"#txtName",
+			"#errName",
+			/^([A-Z][a-z]+.)+$/,
+			"Họ và tên không được để trống!",
+			"Họ và tên không hợp lệ!"
+		);
+    }
+
+    function checkNum () {
+        return validActions(
+			"#txtNum",
+			"#errNum",
+			/^\b(0*(?:[1-9][0-9]?|100))\b$/,
+			"Số áo không được để trống!",
+			"Số áo không hợp lệ!"
+		);
+    }
+
+    function checkClb () {
+        return validActions(
+			"#txtClb",
+			"#errClb",
+			/^([A-Z][a-z]+.)+ [0-9]+$/,
+			"Câu lạc bộkhông được để trống!",
+			"Câu lạc bộ không hợp lệ!"
+		);
+    }
+
+    function checkDate () {
+		
+        var today = new Date();
+		today.setHours(0,0,0,0);
+		today.setDate(today.getDate() + 7);
+        var val = $("#txtDate").val();
+		var date = new Date(val);
+        var errorName = $("#errDate");
+        if (val == "" || val == null) {
+            errorName.html("Ngày tập trung không được trống");
+            return false;
+        } else if (date < today) {
+            errorName.html("Ngày tập trung phải cách ngày hiện tại 7 ngày");
             return false;
         } else {
             errorName.html("");
             return true;    // return true if username is valid
         }
     }
-    $("#txtName").blur(checkName);
 
-    // validation identity card
-    function checkIdentityCard () {
-        var identityCard = $("#txtIdentityCard").val();
-        var errorIdentityCard = $("#error_identityCard");
-        var regexIdentityCard = /^[0-9]{9}$/;
-        if (identityCard == "" || identityCard == null) {
-            errorIdentityCard.html("Số CMND không được để trống!");
-            return false;
-        }
-        else if (!regexIdentityCard.test(identityCard)) {
-            errorIdentityCard.html("Số CMND không hợp lệ!");
-            return false;
-        }
-        else {
-            errorIdentityCard.html("");
-            return true;
-        }
-    }
-    $("#txtIdentityCard").blur(checkIdentityCard);
-
-    // validation txtProvince
-    function checkProvince () {
-        var province = $("#txtProvince").val();
-        var errorProvince = $("#error_province");
-        var regexProvince = /^[A-Z][a-zA-Z]*(\s[A-Z][a-zA-Z]*)*$/;
-        if (province == "" || province == null) {
-            errorProvince.html("Tỉnh/Thành phố không được để trống!");
-            return false;
-        }
-        else if (!regexProvince.test(province)) {
-            errorProvince.html("Tỉnh/Thành phố không hợp lệ!");
-            return false;
-        } else {
-            errorProvince.html("");
-            return true;    // return true if username is valid
-        }
-    }
-    $("#txtProvince").blur(checkProvince);
-
-    // validation email
-    function checkEmail () {
-        var email = $("#txtEmail").val();
-        var errorEmail = $("#error_email");
-        var regexEmail = /^[A-Za-z0-9]+([_\.\-]?[A-Za-z0-9])*@[A-Za-z0-9]+([\.\-]?[A-Za-z0-9]+)*(\.[A-Za-z]+)+$/;
-        if (email == "" || email == null) {
-            errorEmail.html("Email không được để trống!");
-            return false;
-        }
-        else if (!regexEmail.test(email)) {
-            errorEmail.html("Email không hợp lệ!");
-            return false;
-        }
-        else {
-            errorEmail.html("");
-            return true;
-        }
-    }
-    $("#txtEmail").blur(checkEmail);
 
     // validation phone
-    function checkPhone () {
-        var phone = $("#txtPhone").val();
-        var errorPhone = $("#error_phone");
-        var regexPhone = /^[0][0-9]{2}\-[0-9]{3}\-[0-9]{4}$/;
-        if (phone == "" || phone == null) {
-            errorPhone.html("Số điện thoại không được để trống!");
-            return false;
-        }
-        else if (!regexPhone.test(phone)) {
-            errorPhone.html("Số điện thoại không hợp lệ!");
-            return false;
-        }
-        else {
-            errorPhone.html("");
-            return true;
-        }
+    function checkPhone () {		
+        return validActions(
+			"#txtPhone",
+			"#errPhone",
+			/^[0][0-9]{2}\-[0-9]{3}\-[0-9]{4}$/,
+			"Số điện thoại không được để trống!",
+			"Số điện thoại không hợp lệ!"
+		);
     }
-    $("#txtPhone").blur(checkPhone);
 
     // validation image
     function checkImage () {
-        var image = $("#txtImage").val();
-        var regexImage = /(http(s?):)|([/|.|\w|\s])*\.(?:jpg|gif|png)/;
-        var errorImage = $("#error_image");
-        if (image == "" || image == null) {
-            errorImage.html("Hình ảnh không được để trống!");
-            return false;
-        } else if (regexImage.test(image) == false) {
-            errorImage.html("Hình ảnh không hợp lệ!");
-            return false;
-        } else {
-            errorImage.html("");
-            return true;    // return true if username is valid
-        }
+		
+        return validActions(
+			"#txtImage",
+			"#errImage",
+			/(http(s?):)|([/|.|\w|\s])*\.(?:jpg|gif|png)/,
+			"Hình ảnh không được để trống!",
+			"Hình ảnh không hợp lệ!"
+		);
     }
-    $('#txtImage').blur(checkImage);
 
     // check submit
     var i = 1;
     $("#btnSave").click(function () {
         var name = $("#txtName").val();
-        var identityCard = $("#txtIdentityCard").val();
-        var province = $("#txtProvince").val();
-        var email = $("#txtEmail").val();
-        var phone = $("#txtPhone").val();
-        var image = $("#txtImage").val();
-        if (name == '' || identityCard == '' || province == '' || email == '' || phone == '' || image == '') {
-            $('#message').html('Vui lòng nhập đúng và đầy đủ thông tin!');
-            return false;
-        }else {
-            var add = "<tr><th scope=`row`>"+ (i++) +"</th><td>"+ name +"</td><td>"+ identityCard +"</td><td>"+ province +"</td><td>"+ email +"</td><td>"+ phone +"</td><td>"+ image +"</td></tr>";
+		var num = $("#txtNum").val();
+		var clb = $("#txtClb").val();
+		var date = $("#txtDate").val();
+		var phone = $("#txtPhone").val();
+		var image = $('#txtImage').val();
+
+		if (checkName() &&
+			checkNum() &&
+			checkClb() &&
+			checkDate() &&
+			checkPhone() &&
+			checkImage()
+		) {
+            var add = "<tr><th scope=`row`>"+ (i++)
+				+"</th><td>"+ name
+				+"</td><td>"+ num
+				+"</td><td>"+ clb
+				+"</td><td>"+ date
+				+"</td><td>"+ phone
+				+"</td><td>"+ image
+				+"</td></tr>";
             $("tbody").append(add);
             $("#myModal").modal("hide");
             console.log("Đã bắt sự kiện!");
             return true;
+        }else {
+			
+            $('#message').html('Vui lòng nhập đúng và đầy đủ thông tin!');
+            return false;
         }
     });
 });
